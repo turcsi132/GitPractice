bd3d4c0d03dc451f8b053fa13fd0884d954cecca
Zdeněk Janák <janak@astronomie.cz>
2023-02-13 14:21:42 +0100

    b6

--- memberA.java
+++ memberA.java
@@ -3,6 +3,6 @@ public class memberA {
         System.out.println("Greetings");
         
     public static void greet (String parameter){
-        System.out.println(parameter);
+        System.out.println("Greetings for the " + parameter);
     }
 }
