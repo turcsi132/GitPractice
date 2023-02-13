32f0be5fc9b551703c9a4adadbef205d6e61e0ae
unknown <nemetheeszter@gmail.com>
2023-02-13 14:26:46 +0100

    a5

--- memberA.java
+++ memberA.java
@@ -1,7 +1,5 @@
 public class memberA {
-    public static void greet (){
-        System.out.println("Greetings");
-        
+
     public static void greet (String parameter){
         System.out.println(parameter);
     }
