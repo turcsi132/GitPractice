fa7273ac43d8c8f58d51a193fcc8d97b8f3cc4dc
Kulcs Viktor <kulcsviktor@gmail.com>
2023-02-13 14:30:02 +0100

    d6

--- memberA.java
+++ memberA.java
@@ -1,8 +1,10 @@
 public class memberA {
     public static void greet (){
         System.out.println("Greetings");
-        
+    }
+
     public static void greet (String parameter){
         System.out.println(parameter);
     }
+    // blbalblajblajbla comment
 }
