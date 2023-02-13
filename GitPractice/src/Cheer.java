e1104aa4eedb24a24410972be112bf6bd44d9886
unknown <nemetheeszter@gmail.com>
2023-02-13 14:32:42 +0100

    a6

--- GitPractice/src/Cheer.java
+++ GitPractice/src/Cheer.java
@@ -3,13 +3,10 @@ public class Cheer {
 
         cheer();
     }
-    public static String cheer(){
+    public static void cheer(){
         String teamName = "Team 4 BranchingOut";
         String anotherTeamName = "Dojo6 Team";  //modified this parameter name, because i created it earlier unfortunately...
-        String language = "java 4 Ever";
-        System.out.println(anotherTeamName);
-        System.out.println(language);
-        System.out.println(teamName);
-        return anotherTeamName;
+        String language = "java 4 Ever and devops";
+        System.out.println("Cheer for the parameter");
     }
 }
