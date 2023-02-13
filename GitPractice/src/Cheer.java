62a4c513f879bdd76678f51c89aee90b5b2a567a
turcsi132 <turcsi132@yahoo.com>
2023-02-13 14:05:49 +0100

    c5

--- GitPractice/src/Cheer.java
+++ GitPractice/src/Cheer.java
@@ -4,10 +4,12 @@ public class Cheer {
         cheer();
     }
     public static String cheer(){
+        String teamName = "Team 4 BranchingOut";
         String anotherTeamName = "Dojo6 Team";  //modified this parameter name, because i created it earlier unfortunately...
         String language = "java 4 Ever";
         System.out.println(anotherTeamName);
         System.out.println(language);
+        System.out.println(teamName);
         return anotherTeamName;
     }
 }
