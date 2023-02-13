ba3bcc146543af71b28591e35220ddb0bf5ab7bc
Zdeněk Janák <janak@astronomie.cz>
2023-02-13 13:51:57 +0100

    b3

--- GitPractice/src/Cheer.java
+++ GitPractice/src/Cheer.java
@@ -5,7 +5,9 @@ public class Cheer {
     }
     public static String cheer(){
         String anotherTeamName = "Dojo6 Team";  //modified this parameter name, because i created it earlier unfortunately...
+        String language = "java 4 Ever";
         System.out.println(anotherTeamName);
+        System.out.println(language);
         return anotherTeamName;
     }
 }
